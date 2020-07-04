fe783919965bd5be1207c0a414884574cca21329
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-07-04 22:37:40 +0200

    persistence layer

--- src/main/java/com/example/demo/Multiplication.java
+++ src/main/java/com/example/demo/Multiplication.java
@@ -5,16 +5,19 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
+/**
+ * This class represents a Multiplication (a * b).
+ */
+@RequiredArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
-@RequiredArgsConstructor
 @Entity
 public final class Multiplication {
 
@@ -22,8 +25,13 @@ public final class Multiplication {
     @GeneratedValue
     @Column(name = "MULTIPLICATION_ID")
     private Long id;
-    
-    private int factorA;
-    private int factorB;
-    
+
+    // Both factors
+    private final int factorA;
+    private final int factorB;
+
+    // Empty constructor for JSON/JPA
+    Multiplication() {
+        this(0, 0);
+    }
 }
--- src/main/java/com/example/demo/MultiplicationService.java
+++ src/main/java/com/example/demo/MultiplicationService.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+
+import java.util.List;
+
+
 public interface MultiplicationService {
     Multiplication createRandomMultiplication();
 
     boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+
+    List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
 }
