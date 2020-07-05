988309fbc1243beb4233c82cd983eab568437f93
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-07-05 02:45:55 +0200

    jpa

--- src/main/java/com/example/demo/MultiplicationResultAttempt.java
+++ src/main/java/com/example/demo/MultiplicationResultAttempt.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import java.util.List;
 
+import javax.persistence.*;
 
+/**
+ * Identifies the attempt from a {@link User} to solve a
+ * {@link Multiplication}.
+ */
 @RequiredArgsConstructor
 @Getter
 @ToString
@@ -25,25 +22,24 @@ public final class MultiplicationResultAttempt {
     @Id
     @GeneratedValue
     private Long id;
-    
-    @ManyToMany(cascade=CascadeType.PERSIST)
-    @JoinColumn(name="USER_ID")
-    private final List<User> user;
 
-    @ManyToMany(cascade=CascadeType.PERSIST)
-    @JoinColumn(name="MULTIPLICATION_ID")
-    private final List<Multiplication> multiplication;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "USER_ID")
+    private final User user;
 
-    
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "MULTIPLICATION_ID")
+    private final Multiplication multiplication;
     private final int resultAttempt;
+
     private final boolean correct;
 
-
-    // MultiplicationResultAttempt() {
-    //     user = null;
-    //     multiplication = null;
-    //     resultAttempt = -1;
-    //     correct = false;
-    // }
+    // Empty constructor for JSON/JPA
+    MultiplicationResultAttempt() {
+        user = null;
+        multiplication = null;
+        resultAttempt = -1;
+        correct = false;
+    }
 
 }
--- src/test/java/com/example/demo/MultiplicationServiceTest.java
+++ src/test/java/com/example/demo/MultiplicationServiceTest.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito.BDDMyOngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
-
+@SpringBootTest
 public class MultiplicationServiceTest {
     
     @MockBean
@@ -18,9 +20,10 @@ public class MultiplicationServiceTest {
 
     @Test
     public void createRandomMultiplicationTest() {
-        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+        final BDDMyOngoingStubbing<Integer> willReturn = given(randomGeneratorService.generateRandomFactor())
+                .willReturn(50, 30);
 
-        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+        final Multiplication multiplication = multiplicationService.createRandomMultiplication();
 
         assertThat(multiplication.getFactorA()).isEqualTo(50);
         assertThat(multiplication.getFactorB()).isEqualTo(30);
