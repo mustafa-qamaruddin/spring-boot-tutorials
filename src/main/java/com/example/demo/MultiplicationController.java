c25b5fda50f736cd6fc0d50bc4cb67313a2ccb41
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-07-13 02:39:33 +0200

    eureka service discovery and ribbon load balancing

--- src/main/java/com/example/demo/MultiplicationController.java
+++ src/main/java/com/example/demo/MultiplicationController.java
@@ -6,22 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/multiplications")
 final class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
     @Autowired
     public MultiplicationController(
-        final MultiplicationService multiplicationService
+        final MultiplicationService multiplicationService,
+        @Value("${server.port}") final int serverPort
     ) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
     
     @GetMapping("/random")
     Multiplication getRandoMultiplication() {
+        log.info("Multiplication Server @ {}", serverPort);
         return multiplicationService.createRandomMultiplication();
     }
 }
